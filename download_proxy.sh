0674d4655c8b134c3266c9c4cbcae66c73cb0321
svilex <svilex@users.noreply.github.com>
2019-08-07 00:25:20 +0200

    download_proxy

---
+++ download_proxy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+ARCH="$(dpkg --print-architecture)"
+
+case $ARCH in
+    "aarch64")
+        LINK="https://raw.githubusercontent.com/svilex/res/master/proxy_arm64"
+        ;;
+    "arm")
+        LINK="https://raw.githubusercontent.com/svilex/res/master/proxy_arm"
+        ;;
+    *)
+        echo "Architecture not supported yet, contact svile"
+        exit 1
+        ;;
+esac
+
+command -v curl >/dev/null 2>&1 || { echo >&2 "Install curl and retry"; exit 1; }
+
+echo "Downloading proxy..."
+curl "$LINK" --progress-bar -o proxy && chmod +x proxy && echo "Done! Run it using ./proxy address:port"
